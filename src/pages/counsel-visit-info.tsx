a6c6e40710ee2e2fd3dba12d7dfb235c23465fa6
Eun deok Lee <pleed0215@gmail.com>
2022-02-08 14:31:12 +0900

    ...

--- src/pages/counsel-visit-info.tsx
+++ src/pages/counsel-visit-info.tsx
@@ -5,6 +5,7 @@ const CounselVisitInfo = () => {
     return (
         <Layout menuInfo={"진료 안내 > 상담 및 방문 안내"} useHero pageTitle={"진료"}>
             <Seo title={"상담 및 방문 안내"} description={"월배요양병원 상담 및 방문 안내"}/>
+            <div>Hello</div>
         </Layout>
     )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,14 +11,7 @@ const IndexPage = () => (
         <h1 className={"font-godo"}>Hi people 안녕</h1>
         <p>Welcome to your new Gatsby site.</p>
         <p>Now go build something great.</p>
-        <StaticImage
-            src="../images/gatsby-astronaut.png"
-            width={300}
-            quality={95}
-            formats={["auto", "webp", "avif"]}
-            alt="A Gatsby astronaut"
-            style={{ marginBottom: `1.45rem` }}
-        />
+
         <p>
             <Link to="/page-2/">Go to page 2</Link> <br />
             <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
