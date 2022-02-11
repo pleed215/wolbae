6df6c0bf2bc44422bb283bdda1499a47fffc8d90
Eun Deok, Lee <pleed0215@gmail.com>
2022-02-11 09:54:17 +0900

    deploying test2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,26 +6,32 @@ import Layout from "../components/layout"
 import Seo from "../components/seo"
 import { Helmet } from "react-helmet"
 
-const IndexPage = () => (
-    <Layout>
-        <Helmet>
-            <script
-                src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=4fc661cc5efee39dc7d901aabff3c1b9&autoload=false`}
-                defer
-            />
-        </Helmet>
-        <Seo title="Home" />
-        <h1 className={"font-godo"}>Hi people 안녕</h1>
-        <p>Welcome to your new Gatsby site.</p>
-        <p>Now go build something great.</p>
+const IndexPage = () =>
+    process.env.NODE_ENV === "development" ? (
+        <Layout>
+            <Helmet>
+                <script
+                    src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=4fc661cc5efee39dc7d901aabff3c1b9&autoload=false`}
+                    defer
+                />
+            </Helmet>
+            <Seo title="Home" />
+            <h1 className={"font-godo"}>Hi people 안녕</h1>
+            <p>Welcome to your new Gatsby site.</p>
+            <p>Now go build something great.</p>
 
-        <p>
-            <Link to="/page-2/">Go to page 2</Link> <br />
-            <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
-            <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
-            <Link to="/using-dsg">Go to "Using DSG"</Link>
-        </p>
-    </Layout>
-)
+            <p>
+                <Link to="/page-2/">Go to page 2</Link> <br />
+                <Link to="/using-typescript/">
+                    Go to "Using TypeScript"
+                </Link>{" "}
+                <br />
+                <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
+                <Link to="/using-dsg">Go to "Using DSG"</Link>
+            </p>
+        </Layout>
+    ) : (
+        <div>공사중...</div>
+    )
 
 export default IndexPage
