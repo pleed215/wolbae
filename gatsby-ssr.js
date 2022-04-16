504f6c16a450376b1706d8f1df563a88620ea58e
Eun Deok, Lee <pleed0215@gmail.com>
2022-04-16 17:33:49 +0900

    스크립트 load 문제 해결

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,18 @@
  */
 
 // You can delete this file if you're not using it
+import React from "react"
+
+export const onRenderBody = ({ setPostBodyComponents }) => {
+    setPostBodyComponents([
+        <script
+            key={
+                "//dapi.kakao.com/v2/maps/sdk.js?appkey=4fc661cc5efee39dc7d901aabff3c1b9&"
+            }
+            src={
+                "//dapi.kakao.com/v2/maps/sdk.js?appkey=4fc661cc5efee39dc7d901aabff3c1b9&?autoload=false"
+            }
+            defer
+        />,
+    ])
+}
